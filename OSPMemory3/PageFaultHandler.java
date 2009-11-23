62b1ff2dba109fc983a48ea3a1567c7675d1c469
theburek <theburek@54b5dd14-cf81-11de-90e1-e379c62a6cce>
2009-11-23 22:36:13 +0000

    Votever

---
+++ OSPMemory3/PageFaultHandler.java
@@ -0,0 +1,183 @@
+package osp.Memory;
+import java.util.*;
+import osp.Hardware.*;
+import osp.Threads.*;
+import osp.Tasks.*;
+import osp.FileSys.FileSys;
+import osp.FileSys.OpenFile;
+import osp.IFLModules.*;
+import osp.Interrupts.*;
+import osp.Utilities.*;
+import osp.IFLModules.*;
+
+/**
+    The page fault handler is responsible for handling a page
+    fault.  If a swap in or swap out operation is required, the page fault
+    handler must request the operation.
+
+    @OSPProject Memory
+*/
+public class PageFaultHandler extends IflPageFaultHandler
+{
+    /**
+        This method handles a page fault. 
+
+        It must check and return if the page is valid, 
+
+        It must check if the page is already being brought in by some other
+	thread, i.e., if the page's has already pagefaulted
+	(for instance, using getValidatingThread()).
+        If that is the case, the thread must be suspended on that page.
+        
+        If none of the above is true, a new frame must be chosen 
+        and reserved until the swap in of the requested 
+        page into this frame is complete. 
+
+	Note that you have to make sure that the validating thread of
+	a page is set correctly. To this end, you must set the page's
+	validating thread using setValidatingThread() when a pagefault
+	happens and you must set it back to null when the pagefault is over.
+
+        If a swap-out is necessary (because the chosen frame is
+        dirty), the victim page must be dissasociated 
+        from the frame and marked invalid. After the swap-in, the 
+        frame must be marked clean. The swap-ins and swap-outs 
+        must are preformed using regular calls read() and write().
+
+        The student implementation should define additional methods, e.g, 
+        a method to search for an available frame.
+
+	Note: multiple threads might be waiting for completion of the
+	page fault. The thread that initiated the pagefault would be
+	waiting on the IORBs that are tasked to bring the page in (and
+	to free the frame during the swapout). However, while
+	pagefault is in progress, other threads might request the same
+	page. Those threads won't cause another pagefault, of course,
+	but they would enqueue themselves on the page (a page is also
+	an Event!), waiting for the completion of the original
+	pagefault. It is thus important to call notifyThreads() on the
+	page at the end -- regardless of whether the pagefault
+	succeeded in bringing the page in or not.
+
+        @param thread the thread that requested a page fault
+        @param referenceType whether it is memory read or write
+        @param page the memory page 
+
+	@return SUCCESS is everything is fine; FAILURE if the thread
+	dies while waiting for swap in or swap out or if the page is
+	already in memory and no page fault was necessary (well, this
+	shouldn't happen, but...). In addition, if there is no frame
+	that can be allocated to satisfy the page fault, then it
+	should return NotEnoughMemory
+
+        @OSPProject Memory
+    */
+    public static int do_handlePageFault(ThreadCB thread, 
+					 int referenceType,
+					 PageTableEntry page)
+    {
+        if(page.isValid()) 
+        {
+        	page.notifyThreads();
+        	ThreadCB.dispatch();
+        	return FAILURE;	// Ako je vec u memoriji
+        }
+        
+        if(page.getValidatingThread() == thread)
+        {
+        	MyOut.print("do_handlePageFault", "Shouldn't happen.");
+        	return SUCCESS;
+        }
+    	
+        FrameTableEntry frame = findUnlockedFrame();
+        if(frame == null)
+        {
+        	page.notifyThreads();
+        	ThreadCB.dispatch();
+        	return NotEnoughMemory;	// Ako nema slobodnih frejmova
+        }
+        
+        //if(thread.getStatus() == ThreadKill) {return FAILURE; // Thread poginuo u medjuvremenu
+        
+        frame.setReserved(thread.getTask()); // Rezervisanje pejdza
+        
+        SystemEvent suspendEvent = new SystemEvent("PageFault");
+        page.setValidatingThread(thread);
+        thread.suspend(suspendEvent); // Obustavljanje threada dok se swappuje
+        if(thread.getStatus() == ThreadKill) 
+        {
+        	page.notifyThreads();
+        	ThreadCB.dispatch();
+        	return FAILURE; // Opet mogucnost izgibije
+        }
+    	
+        
+        if(frame.getPage() != null)	// Ako vec ima stranica u okviru
+        {
+        	if(frame.isDirty())	// Ako je perverzna
+        	{
+        		thread.getTask().getSwapFile().write(frame.getID(),	// Swap-out 
+        				frame.getPage(), thread);
+        		frame.getPage().setValid(false);
+        		frame.getPage().setFrame(null);
+        		
+        		if(thread.getStatus() == ThreadKill) 
+        		{
+        			page.setValidatingThread(null);
+        			page.notifyThreads();
+                	ThreadCB.dispatch();
+        			return FAILURE; // ...
+        		}
+        	}
+        	frame.setPage(null);	// Cisto
+        	frame.setDirty(false);
+        }
+        
+        frame.setPage(page);
+        page.setFrame(frame);
+        thread.getTask().getSwapFile().read(frame.getID(),
+        		page, thread);	// Swap-in
+        page.setValid(true);
+        
+        if(thread.getStatus() == ThreadKill) 
+        {
+        	page.setValidatingThread(null);
+        	page.notifyThreads();
+        	ThreadCB.dispatch();
+        	return FAILURE; // ?
+        }
+        
+        frame.setReserved(null); // Odrezervisanje
+        frame.setDirty(referenceType == MemoryWrite);
+        
+        page.setValidatingThread(null);
+        suspendEvent.notifyThreads();
+        page.notifyThreads();
+        ThreadCB.dispatch();
+        
+    	return SUCCESS;
+
+    }
+    
+    private static FrameTableEntry findUnlockedFrame()
+    {
+    	for(FrameTableEntry f : MMU.frameTable)
+    	{
+    		if(!f.isReserved() && !f.isReferenced())
+    		{
+    			return f;
+    		}
+    	}
+    	return null;
+    }
+
+
+    /*
+       Feel free to add methods/fields to improve the readability of your code
+    */
+
+}
+
+/*
+      Feel free to add local classes to improve the readability of your code
+*/
---
+++ OSPMemory3/PageTable.java
@@ -0,0 +1,77 @@
+package osp.Memory;
+/**
+    The PageTable class represents the page table for a given task.
+    A PageTable consists of an array of PageTableEntry objects.  This
+    page table is of the non-inverted type.
+
+    @OSPProject Memory
+*/
+import java.lang.Math;
+import osp.Tasks.*;
+import osp.Utilities.*;
+import osp.IFLModules.*;
+import osp.Hardware.*;
+
+public class PageTable extends IflPageTable
+{
+	int maxPages = 0;
+	static int absPage = 0;
+    /** 
+	The page table constructor. Must call
+	
+	    super(ownerTask)
+
+	as its first statement.
+
+	@OSPProject Memory
+    */
+    public PageTable(TaskCB ownerTask)
+    {
+    	super(ownerTask);
+    	
+    	maxPages = (int) Math.pow(2, MMU.getPageAddressBits()); // Broj stranica
+    	super.pages = new PageTableEntry[maxPages]; // Inicijalizacija arraya
+    	
+    	for(int i = 0; i < maxPages; ++i) // Konstruisanje svake, dijeli im se po redni broj
+    	{
+    		//super.pages[i] = new PageTableEntry(this, ++PageTable.absPage);
+    		super.pages[i] = new PageTableEntry(this, i);
+    	}
+
+    }
+
+    /**
+       Frees up main memory occupied by the task.
+       Then unreserves the freed pages, if necessary.
+
+       @OSPProject Memory
+    */
+    public void do_deallocateMemory()
+    {
+    	TaskCB task = super.getTask();
+    	for(int i = maxPages - 1; i >= 0; --i)
+    	{
+    		if(pages[i].isValid())
+    		{
+    			if(pages[i].getFrame().getReserved() == task)
+    			{
+    				pages[i].getFrame().setReserved(null);
+    			}
+    			
+    			pages[i].getFrame().setDirty(true);
+    			pages[i].getFrame().setReferenced(false);
+    			pages[i].getFrame().setPage(null);
+    		}
+    	}
+    	
+    }
+
+    /*
+       Feel free to add methods/fields to improve the readability of your code
+    */
+
+}
+
+/*
+      Feel free to add local classes to improve the readability of your code
+*/

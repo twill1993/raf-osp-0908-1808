d23d6b272606556559bcd42572a7f81b5350d671
marija.prokopijevic <marija.prokopijevic@54b5dd14-cf81-11de-90e1-e379c62a6cce>
2009-11-23 18:11:28 +0000

    Gotove metode do_lock i do_unlock

--- OSPMemory/src/osp/Memory/PageTableEntry.java
+++ OSPMemory/src/osp/Memory/PageTableEntry.java
@@ -1,10 +1,8 @@
 package osp.Memory;
 
-import osp.Hardware.*;
-import osp.Tasks.*;
+
 import osp.Threads.*;
 import osp.Devices.*;
-import osp.Utilities.*;
 import osp.IFLModules.*;
 /**
    The PageTableEntry object contains information about a specific virtual
@@ -48,14 +46,36 @@ public class PageTableEntry extends IflPageTableEntry
     //goal of this method is to increment the lock count of the fram associated with the page.
     public int do_lock(IORB iorb)
     {
+    	//first increment the lock count
+    	this.getFrame().incrementLockCount();
+    
+    	//first check if the page is in main memory by testing the validity bit of the page
     	if(!isValid())
     	{
+    		//If the page is invalid, a pagefault must be initiated.
     		PageFaultHandler.handlePageFault(iorb.getThread(), iorb.getDeviceID(), this);
+    		return FAILURE;
     	}
+    	//To help identify the pages that are involved in a pagefault, OSP 2 provides the method getValidatingThread()
+    	//this method returns the thread that caused a pagefault on that page
+    	if(iorb.getThread() == null || getValidatingThread() == iorb.getThread())
+    	{
+    		//If Th2 = Th1, then the proper action is to return right after incrementing the lock count
+    		return SUCCESS;
+    	}
+    	if(iorb.getThread() != null || getValidatingThread() != iorb.getThread()){
+    		//If Th2 != Th1 execute the suspend() method on Th2 and pass page P as a parameter
+			ThreadCB th = iorb.getThread();
+			th.suspend(this);
+			if(isValid()){
+				return SUCCESS;
+				//SUCCESS if the page became valid as a result of the pagefault 
+			}
+			//and FAILURE otherwise.
+			return FAILURE;
+		}
     
-    	this.getFrame().incrementLockCount();
-    	int lockCount = this.getFrame().getLockCount();
-    	return lockCount;
+    	return SUCCESS;
     }
 
     /** This method decreases the lock count on the page by one. 
@@ -66,6 +86,7 @@ public class PageTableEntry extends IflPageTableEntry
     */
     public void do_unlock()
     {
+    	//lock count ne sme da padne ispod nule 
     	if(this.getFrame().getLockCount()  > 0)
     	{
     		this.getFrame().decrementLockCount();

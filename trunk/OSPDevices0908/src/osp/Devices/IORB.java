bae95781b33d628086207b77cea0104002ab7ac5
theburek <theburek@54b5dd14-cf81-11de-90e1-e379c62a6cce>
2009-11-30 12:55:06 +0000

    Votever.

--- trunk/OSPDevices0908/src/osp/Devices/IORB.java
+++ trunk/OSPDevices0908/src/osp/Devices/IORB.java
@@ -25,7 +25,7 @@ public class IORB extends IflIORB
     public IORB(ThreadCB thread, PageTableEntry page, 
 		int blockNumber, int deviceID, 
 		int ioType, OpenFile openFile) {
-        // your code goes here
+        super(thread, page, blockNumber, deviceID, ioType, openFile);
 
     }
 
